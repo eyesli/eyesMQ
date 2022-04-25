b3020ab9c76feabad111a983f0b7dace11d1f9f1
Levi Li <levil@synnex.com>
2022-04-25 18:04:42 +0800

    增加一个模块用来做测试

---
+++ design/src/test/java/com/levil/eyesmq/BeanTest.java
@@ -0,0 +1,26 @@
+package com.levil.eyesmq;
+
+import com.levil.design.DesignApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DesignApplication.class)
+public class BeanTest {
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @Test
+    public void testAdd() {
+        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println("beanDefinitionName = " + beanDefinitionName);
+        }
+
+
+    }
+}
